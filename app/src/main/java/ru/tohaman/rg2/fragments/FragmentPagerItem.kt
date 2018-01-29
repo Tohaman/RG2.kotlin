6a3e049d6377ac5c5c7402b72db1766c8dbfc418
Tohaman <avrozovu@yandex.ru>
2018-01-29 22:40:11 +0300

    Home: Поправлен 1 этап для пирамидки (не доделан)

--- app/src/main/java/ru/tohaman/rg2/fragments/FragmentPagerItem.kt
+++ app/src/main/java/ru/tohaman/rg2/fragments/FragmentPagerItem.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.preference.PreferenceManager
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
-import android.text.Editable
 import android.text.Html
 import android.text.InputType
 import android.text.method.LinkMovementMethod
