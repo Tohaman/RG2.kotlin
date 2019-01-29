c3d1c6325a188dbca546a7e064156f6aae47c195
Tohaman <avrozovu@yandex.ru>
2019-01-29 14:54:44 +0300

    Add developer keys

--- app/src/main/java/ru/tohaman/rg2/DeveloperKey.kt
+++ app/src/main/java/ru/tohaman/rg2/DeveloperKey.kt
@@ -1,7 +1,11 @@
 package ru.tohaman.rg2
 
 /**
- * Static container class for holding a reference to your YouTube Developer Key.
+ * Static container class for holding a reference to your Developer Key.
+ * need to add this key in your gradle.properties
+ * YouTube_DEVELOPER_KEY = "ХХХХХХХХХХХХХХХХХХХХХХХХ"
+ * RG2_Kotlin_GooglePlayKey = "XXXXXXXXXXXXXXXXXXXXXXX"
+ * https://medium.com/code-better/hiding-api-keys-from-your-android-repository-b23f5598b906
  */
 object DeveloperKey {
 
@@ -11,12 +15,11 @@ object DeveloperKey {
      * [Google Developers Console](https://console.developers.google.com/)
      * to register a new developer key.
      */
-    val DEVELOPER_KEY = "AIzaSyCtccTdfmn7HawTUzVKOzqUPQPdthrO4VA"
+    val DEVELOPER_KEY = BuildConfig.YouTubeApiKey
 
     /**
-     * Ключик для приема оплаты внутри приложения
+     * открытый ключ RSA для приема оплаты внутри приложения
      */
-    val base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApehfI/wiNweAnmThLr6hkcNR/OKaWqb8H1bjkHoFEx5vO0GaliQbxUhrwr4eu4Nu2/Fyad7QKv/Iel/S+bhaorAHcNChH3Miw0PEXBS2KWNdCMDedVcul1ltyPDdtuNXU/taIJtPldjhB71G5UrQRXI/0XogYSyVC4bFUftbewc+xeDQp3myt1uI2EeHaOhM39nTdqjunJNaldlFPPWoU3a8bg3jFu45BWqK5f9TRQFWllVdU98rKJXN9tbctQLih05n3mmXwfJKSjZCHFdUGStyxcDQFXY8cTeFioeoTBs70ER8L3i8gHsb8YYEFueITId9RqH+ufv/14nzy+BSBQIDAQAB"
-
+    val base64EncodedPublicKey = BuildConfig.GooglePlayKey
 
 }
