3b5904e0e17720a76abf68160977a78f3c9ac101
Tohaman <avrozovu@yandex.ru>
2019-01-29 16:47:18 +0300

    Add hidden DeveloperKey

---
+++ app/src/main/java/ru/tohaman/rg2/DeveloperKey.kt
@@ -0,0 +1,25 @@
+package ru.tohaman.rg2
+
+/**
+ * Static container class for holding a reference to your Developer Key.
+ * need to add this key in your gradle.properties
+ * YouTube_ApiKey = "ХХХХХХХХХХХХХХХХХХХХХХ"
+ * RG2_Kotlin_GooglePlayKey = "XXXXXXXXXXXXXXXXXXXXX"
+ * https://medium.com/code-better/hiding-api-keys-from-your-android-repository-b23f5598b906
+ */
+object DeveloperKey {
+
+    /**
+     * Please replace this with a valid API key which is enabled for the
+     * YouTube Data API v3 service. Go to the
+     * [Google Developers Console](https://console.developers.google.com/)
+     * to register a new developer key.
+     */
+    val DEVELOPER_KEY = BuildConfig.YouTubeApiKey
+
+    /**
+     * открытый ключ RSA для приема оплаты внутри приложения
+     */
+    val base64EncodedPublicKey = BuildConfig.GooglePlayKey
+
+}
